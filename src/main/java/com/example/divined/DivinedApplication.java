a678d9c827472c2690140ab37cd3f771cd98d643
DYS <kcood92@gmail.com>
2022-08-07 12:24:42 +0900

    Initial commit

---
+++ src/main/java/com/example/divined/DivinedApplication.java
@@ -0,0 +1,13 @@
+package com.example.divined;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivinedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivinedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/divined/DivinedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.divined;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivinedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
